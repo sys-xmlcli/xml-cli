bfb8ba344d01b505df43b975524aa7d7a3e6dbe4
sys-xmlcli <sys_xmlcli@intel.com>
2026-09-25 10:25:35 +0530

    remove world-write on compression binary permissions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ class PostInstall(install):
     if sys.platform.startswith('linux'):  # Just for linux, change the attributes
       for filepath in self.get_outputs():
         if filepath.find("LzmaCompress") > -1 or filepath.find("TianoCompress") > -1 or filepath.find("Brotli") > -1:
-          os.chmod(filepath, 0o755)  # executable, but not world-writable
+          os.chmod(filepath, 0o755)  # executable, but not world-writable 
 
 
 if __name__ == "__main__":
